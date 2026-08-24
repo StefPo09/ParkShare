09cbfc29bf83e3b9e673d32e9fb574df33fda7da
sch1afend <cosminflorea_2108@yahoo.com>
2026-08-24 21:33:25 +0300

    added edit/manage spots pages

---
+++ frontend/app/EditSpotPage/layout.tsx
@@ -0,0 +1,6 @@
+import type { ReactNode } from "react";
+import React from "react";
+
+export default function EditCarPageLayout({ children }: { children: ReactNode }) {
+    return <>{children}</>;
+}
